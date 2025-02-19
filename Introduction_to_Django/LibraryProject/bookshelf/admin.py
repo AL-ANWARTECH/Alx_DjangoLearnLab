44fd49c93df69c4bb318317bc503cbf55945877f
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-02-19 03:39:17 -0800

    Implementing and Interacting with Django Models

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")  # Columns in list view
+    search_fields = ("title", "author") # Enables search bar
+    list_filter = ("publication_year",) # Adds a filter for publication year
+    
