35e48c276acc6a141a1d6c34b8445fdb9321475d
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-03-09 09:01:30 +0100

    Learning API

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -47,6 +47,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication', 
         'rest_framework.authentication.SessionAuthentication', 
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', 
+    ),
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
