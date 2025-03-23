4a06814425fc836580cee0a899b67465422bbcb5
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-03-24 00:15:58 +0100

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -48,3 +48,4 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Write a comment...'}),
         }
+
