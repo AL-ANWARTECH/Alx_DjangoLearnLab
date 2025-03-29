49f8be31f21ecff4b2adb3a6992151200f289ec7
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-03-29 17:47:34 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,65 +1,65 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
 from django.shortcuts import get_object_or_404
 from .models import User
-from .serializers import UserSerializer, CustomTokenObtainPairSerializer
+from .serializers import UserSerializer
 
-# User Registration View
-class RegisterView(generics.CreateAPIView):
-    serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
-
-# Custom Token View for JWT Authentication
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
-
-# User Profile View
-class UserProfileView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        return self.request.user
-
-# Follow User View
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
     
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(User, id=user_id)
-        if request.user.follow(user_to_follow):
-            return Response({'status': f'You are now following {user_to_follow.username}'})
-        return Response(
-            {'error': 'Unable to follow user'}, 
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        # Get all users to find the target user
+        user_to_follow = get_object_or_404(User.objects.all(), id=user_id)
+        
+        if request.user.id == user_to_follow.id:
+            return Response(
+                {'error': 'You cannot follow yourself'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        if request.user.following.filter(id=user_to_follow.id).exists():
+            return Response(
+                {'error': 'You are already following this user'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        request.user.following.add(user_to_follow)
+        return Response({
+            'status': f'You are now following {user_to_follow.username}',
+            'following_count': request.user.following.count()
+        })
 
-# Unfollow User View
 class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
     
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(User, id=user_id)
-        if request.user.unfollow(user_to_unfollow):
-            return Response({'status': f'You have unfollowed {user_to_unfollow.username}'})
-        return Response(
-            {'error': 'Unable to unfollow user'}, 
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        # Get all users to find the target user
+        user_to_unfollow = get_object_or_404(User.objects.all(), id=user_id)
+        
+        if not request.user.following.filter(id=user_to_unfollow.id).exists():
+            return Response(
+                {'error': 'You are not following this user'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        request.user.following.remove(user_to_unfollow)
+        return Response({
+            'status': f'You have unfollowed {user_to_unfollow.username}',
+            'following_count': request.user.following.count()
+        })
 
-# List of Users Being Followed
 class FollowingListView(generics.ListAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
+        # Return all users the current user is following
         return self.request.user.following.all()
 
-# List of Followers
 class FollowersListView(generics.ListAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return self.request.user.followers.all()
+        # Return all users following the current user
+        return User.objects.filter(following=self.request.user)
